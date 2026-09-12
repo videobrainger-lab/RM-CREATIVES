aee99f89a4e157f3ffee2e06590738cde6301194
videobrainger-lab <videobrainger@gmail.com>
2026-09-12 15:25:01 +0300

    Exclude noindex utility pages from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,20 @@ import sitemap from '@astrojs/sitemap';
 // Static production build for rmcreatives.com.
 export default defineConfig({
   site: 'https://rmcreatives.com',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => ![
+        '/thank-you/',
+        '/ru/thank-you/',
+        '/privacy/',
+        '/ru/privacy/',
+        '/terms/',
+        '/ru/terms/',
+        '/cookies/',
+        '/ru/cookies/',
+        '/404/'
+      ].some((path) => page.endsWith(path))
+    })
+  ],
   output: 'static'
 });
